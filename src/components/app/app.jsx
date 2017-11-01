0ab89da8562e9e3ba6dbfd8a195d07b4835dc66a
Lee Saxby <lsaxby@gresham-computing.com>
2017-11-01 10:08:44 +0000

    Update todo reducer variable name to distinguish it from the todo component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { combineReducers } from 'redux';
 
 import Todos from '../todos/todos.jsx';
-import { todos } from '../todos/todos.reducers.js';
+import { todos as todoReducer } from '../todos/todos.reducers.js';
 
-export const rootReducer = combineReducers({ todos });
+export const rootReducer = combineReducers({ todos: todoReducer });
 
 export function App() {
     return (
