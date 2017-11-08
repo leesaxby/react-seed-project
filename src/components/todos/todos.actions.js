7465342061bd656219ff6a34803bd3bffb71d5e6
Lee Saxby <lsaxby@gresham-computing.com>
2017-11-08 13:10:21 +0000

    Create async action test
    Return promise from fetch todo action to simulate fetch request

--- src/components/todos/todos.actions.js
+++ src/components/todos/todos.actions.js
@@ -7,15 +7,23 @@ export function addTodo(todo) {
     };
 }
 
+
 export function fetchTodos() {
     return dispatch => {
-        dispatch({
-            type: 'FETCH_TODOS_SUCCESS',
-            payload: [
-                { _id: 1, text: 'Item One', done: false},
-                { _id: 2, text: 'Item Two', done: false},
-                { _id: 3, text: 'Item Three', done: false}
-            ]
+
+        // Creating promise to simulate promise returned from fetch request.
+        return new Promise((resolve) => {
+            resolve();
+        })
+        .then(() => {
+            dispatch({
+                type: 'FETCH_TODOS_SUCCESS',
+                payload: [
+                    { _id: 1, text: 'Item One', done: false},
+                    { _id: 2, text: 'Item Two', done: false},
+                    { _id: 3, text: 'Item Three', done: false}
+                ]
+            });
         });
     };
 }
