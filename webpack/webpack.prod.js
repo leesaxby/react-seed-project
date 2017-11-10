3308256bd3366e90a95d7384513ace988d615285
Lee Saxby <lsaxby@gresham-computing.com>
2017-11-10 19:20:14 +0000

    Add linting to prod build and fail on lint error
    Add uglify optimisations

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -9,12 +9,27 @@ module.exports = merge(common, {
         path: path.resolve(__dirname, '../dist'),
         filename: '[name].[hash].js',
     },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            enforce: 'pre',
+            loader: 'eslint-loader',
+            exclude: /node_modules/,
+            options: {
+                failOnWarning: false,
+                failOnError: true
+            }
+        }]
+    },
     plugins: [
         new UglifyJSPlugin(),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
-            }
+            },
+            mangle: false,
+            compress: true,
+            comments: false
         })
     ]
 });
