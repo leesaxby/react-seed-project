fce85e160a297941e2f559ffbce8713b8f5e88d4
Lee Saxby <lsaxby@gresham-computing.com>
2017-11-16 10:04:33 +0000

    Add tests
    Add util to check if component is rendered

--- src/components/todos/todos.test.js
+++ src/components/todos/todos.test.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { mountWithIntl } from '../../../test/intl-enzyme-test-uitl.js';
+import { renderedInWrapper } from '../../../test/utils';
 
 import { Todos } from './Todos';
 import TodoFilter from './todoFilter/TodoFilter';
 import TodoList from './todoList/TodoList';
+import TodoItem from './todoItem/TodoItem';
 import TodoForm from './todoForm/TodoForm';
 
 configure({ adapter: new Adapter() });
 
 const props = {
-    listItems: [],
+    listItems: [
+        { _id: 1, text: 'Item 1', done: true }
+    ],
     filter: 'ACTIVE',
     fetchData: jest.fn(),
     addTodo: jest.fn(),
@@ -23,31 +27,56 @@ const wrapper = mountWithIntl(
     <Todos { ...props }/>
   );
 
+const isRendered = renderedInWrapper(wrapper);
+
 describe('Todos component', () => {
 
-    describe('Child components render', function() {
+    describe('Child components render', () => {
 
         it('Should render TodoForm component', () => {
-            expect( wrapper.find(TodoForm).length ).toBe(1);
+            expect( isRendered(TodoForm) ).toBe(true);
         });
 
         it('Should render TodoFilter component', () => {
-            expect( wrapper.find(TodoFilter).length ).toBe(1);
+            expect( isRendered(TodoFilter) ).toBe(true);
         });
 
         it('Should render TodoList component', () => {
-            expect( wrapper.find(TodoList).length ).toBe(1);
+            expect( isRendered(TodoList) ).toBe(true);
+        });
+
+        it('Should render TodoList component', () => {
+            expect( isRendered(TodoItem) ).toBe(true);
         });
 
     });
 
+    describe('Calls correct props', () => {
 
+        it('Should call addTodo prop when addTodoItem method is called', () => {
+            expect(props.addTodo.mock.calls.length).toBe(0);
 
-    // it('Should call onToggleDone when clicked', () => {
-    //     expect(props.onToggleDone.mock.calls.length).toBe(0);
+            wrapper.instance().addTodoItem();
 
-    //     wrapper.find(Todos).simulate('click');
+            expect( props.addTodo.mock.calls.length ).toBe(1);
+        });
 
-    //     expect( props.onToggleDone.mock.calls.length ).toBe(1);
-    // });
+        it('Should call fetchData prop when getTodos method is called', () => {
+            expect(props.fetchData.mock.calls.length).toBe(1);
+
+            wrapper.instance().getTodos();
+
+            expect( props.fetchData.mock.calls.length ).toBe(2);
+        });
+
+        it('Should call fetchData prop when getTodos method is called', () => {
+            expect(props.fetchData.mock.calls.length).toBe(1);
+
+            wrapper.instance().getTodos();
+
+            expect( props.fetchData.mock.calls.length ).toBe(2);
+        });
+
+    });
+
 });
