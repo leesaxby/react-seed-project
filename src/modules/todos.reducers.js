ef0c425be76dfa43510acc58b8f6bd3fd2e0d5e9
Matt Lewis <mlewis@greshamtech.com>
2017-11-21 16:25:58 +0000

    Make reducers respect immutability

--- src/modules/todos.reducers.js
+++ src/modules/todos.reducers.js
@@ -8,26 +8,25 @@ export function todos(state = initialState, action) {
     switch (action.type) {
         case ADD_TODO: {
             const ids = state.listItems.map(todo => todo._id);
-
-            action.payload._id = ids.length ? Math.max(...ids) + 1 : 0;
-
+            const newId = ids.length ? Math.max(...ids) + 1 : 0;
             return {
-                listItems: [...state.listItems, action.payload]
+                listItems: [...state.listItems, Object.assign({}, action.payload, { _id: newId })]
             };
         }
         case FETCH_TODOS_SUCCESS:
             return {
                 listItems: action.payload
             };
-        case UPDATE_DONE_STATUS:
-            {
-                const listItems = Object.assign(state.listItems);
-                listItems.find(({ _id }) => _id === action.payload._id).done = action.payload.doneStatus;
-
-                return {
-                    listItems
-                };
-            }
+        case UPDATE_DONE_STATUS: {
+            const listItems = Object.assign(state.listItems);
+            const itemIndex = listItems.findIndex(({ _id }) => _id === action.payload._id);
+            const newListItem = Object.assign({}, listItems[itemIndex], { 
+                done: action.payload.doneStatus 
+            });
+            return {
+                listItems: Object.assign([], state.listItems, { [itemIndex]: newListItem })
+            };
+        }
         default:
             return state;
     }
