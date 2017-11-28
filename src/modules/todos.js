81af3c1e964a6206d0afaf262104b43f47640f42
Lee Saxby <lsaxby@gresham-computing.com>
2017-11-28 10:53:09 +0000

    Separate logic for updating single todo from updating todos array

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -2,14 +2,14 @@ const ADD_TODO = 'app/todos/ADD_TODO';
 const FETCH_TODOS_SUCCESS = 'app/todos/FETCH_TODOS_SUCCESS';
 const UPDATE_DONE_STATUS = 'app/todos/UPDATE_DONE_STATUS';
 
-const addTodo = (todo) => ({ 
-    type: ADD_TODO, 
-    payload: todo 
+const addTodo = (todo) => ({
+    type: ADD_TODO,
+    payload: todo
 });
 
-const updateDoneStatus = (_id, doneStatus) => ({ 
-    type: UPDATE_DONE_STATUS, 
-    payload: { _id, doneStatus } 
+const updateDoneStatus = (_id, doneStatus) => ({
+    type: UPDATE_DONE_STATUS,
+    payload: { _id, doneStatus }
 });
 
 const fetchTodosSuccess = () => ({
@@ -36,13 +36,35 @@ const initialState = {
     listItems: []
 };
 
-export default function todos(state = initialState, action) {
+const todo = (state ,action) => {
     switch (action.type) {
         case ADD_TODO: {
             const ids = state.listItems.map(todo => todo._id);
             const newId = ids.length ? Math.max(...ids) + 1 : 0;
+            return Object.assign({}, action.payload, { _id: newId });
+        }
+        case UPDATE_DONE_STATUS: {
+            const listItems = Object.assign(state.listItems);
+            const itemIndex = listItems.findIndex(({ _id }) => _id === action.payload._id);
+            const newListItem = Object.assign({}, listItems[itemIndex], {
+                done: action.payload.doneStatus
+            });
+
+            return { [itemIndex]: newListItem };
+        }
+        default:
+            return state;
+    }
+};
+
+export default function todos(state = initialState, action) {
+    switch (action.type) {
+        case ADD_TODO: {
             return {
-                listItems: [...state.listItems, Object.assign({}, action.payload, { _id: newId })]
+                listItems: [
+                    ...state.listItems,
+                    todo(state, action)
+                ]
             };
         }
         case FETCH_TODOS_SUCCESS:
@@ -50,13 +72,8 @@ export default function todos(state = initialState, action) {
                 listItems: action.payload
             };
         case UPDATE_DONE_STATUS: {
-            const listItems = Object.assign(state.listItems);
-            const itemIndex = listItems.findIndex(({ _id }) => _id === action.payload._id);
-            const newListItem = Object.assign({}, listItems[itemIndex], { 
-                done: action.payload.doneStatus 
-            });
             return {
-                listItems: Object.assign([], state.listItems, { [itemIndex]: newListItem })
+                listItems: Object.assign([], state.listItems, todo(state, action))
             };
         }
         default:
