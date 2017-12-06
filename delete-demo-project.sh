61204a7330f4c89ff22e977700d5535924ed145d
Lee Saxby <lsaxby@gresham-computing.com>
2017-12-06 14:51:51 +0000

    Add script to easily remove demo todo project

---
+++ delete-demo-project.sh
@@ -0,0 +1,12 @@
+sed -i -e "/import todos from/d" src/store.js
+sed -i -e 's/combineReducers({ todos })/combineReducers({ })/g' src/store.js
+
+sed -i -e "/import Todos from/d" src/App.js
+sed -i -e 's/<Todos [/]>/<div>Hello World<\/div>/g' src/App.js
+
+rm -rf src/pages/todos
+rm src/pages/Todos.js
+rm src/pages/todos.test.js
+rm src/modules/todos.js
+rm src/modules/todos.test.js
+rm src/images/list.png
