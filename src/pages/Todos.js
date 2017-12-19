ba8c01631331b12399205cd58136e45cb80cd863
Matt Lewis <mlewis@greshamtech.com>
2017-12-19 18:05:26 +0000

    Refactor Todos to allow splitting up of rows into individual components

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -10,6 +10,69 @@ import Filter from './todos/Filter';
 
 import listImage from '../images/list.png';
 
+const HeaderRow = () => (
+    <Row>
+        <Col sm={1} lg={1} smOffset={4} lgOffset={5}>
+            <Image src={ listImage }
+                   alt="List Image"
+                   style={{ 'height': '200px',
+                            'marginBottom': '20px' }}/>
+        </Col>
+    </Row>
+);
+
+const InputRow = ({ filter, updateFilter, addTodo }) => {
+
+    const addTodoItem = (newItem) => addTodo({ text: newItem, done: false });
+
+    return (
+        <Row>
+            <Col sm={8} lg={8} smOffset={1} lgOffset={2}>
+                <Form onAddTodoItem={ addTodoItem } />
+            </Col>
+            <Col sm={3} lg={2}>
+                <Filter filter={ filter }
+                            onUpdateFilter={ updateFilter } />
+            </Col>
+        </Row>
+    );
+};
+
+InputRow.defaultProps = {
+    filter: 'ACTIVE'
+};
+InputRow.propTypes = {
+    filter: PropTypes.oneOf([ 'ACTIVE', 'DONE' ]),
+    addTodo: PropTypes.func.isRequired,
+    updateFilter: PropTypes.func.isRequired
+};
+
+const ListRow = ({ updateDoneStatus, listItems }) => {
+
+    const toggleDone = ({ _id, done }) => updateDoneStatus(_id, !done);
+
+    return (
+        <Row>
+            <Col sm={8} lg={6} smOffset={2} lgOffset={3}>
+                <List listItems={ listItems }
+                          onToggleDone={ toggleDone }/>
+            </Col>
+        </Row>
+    );
+};
+
+ListRow.defaultProps = {
+    listItems: []
+};
+ListRow.propTypes = {
+    listItems: PropTypes.arrayOf(
+        PropTypes.shape({
+            done: PropTypes.bool
+        })
+    ),
+    updateDoneStatus: PropTypes.func.isRequired
+};
+
 export class Todos extends React.Component {
 
     static defaultProps = {
@@ -38,37 +101,14 @@ export class Todos extends React.Component {
         return (
             <div>
             <Grid>
-                <Row>
-                    <Col sm={1} lg={1} smOffset={4} lgOffset={5}>
-                        <Image src={listImage}
-                               alt="List Image"
-                               style={{ 'height': '200px',
-                                        'marginBottom': '20px' }}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col sm={8} lg={8} smOffset={1} lgOffset={2}>
-                        <Form onAddTodoItem={this.addTodoItem} />
-                    </Col>
-                    <Col sm={3} lg={2}>
-                        <Filter filter={this.props.filter}
-                                    onUpdateFilter={this.props.updateFilter} />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col sm={8} lg={6} smOffset={2} lgOffset={3}>
-                        <List listItems={this.props.listItems}
-                                  onToggleDone={this.toggleDone}/>
-                    </Col>
-                </Row>
+                <HeaderRow />
+                <InputRow filter={this.props.filter} updateFilter={this.props.updateFilter} addTodo={this.props.addTodo} />
+                <ListRow updateDoneStatus={this.props.updateDoneStatus} listItems={this.props.listItems} />
             </Grid>
             </div>
         );
     }
 
-    addTodoItem = (newItem) => this.props.addTodo({ text: newItem, done: false });
-
-    toggleDone = ({ _id, done }) => this.props.updateDoneStatus(_id, !done);
 }
 
 const filterTodos = (list, filter) => {
