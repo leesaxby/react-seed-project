5589d45a1256ed668bbe1adb26a61376d122aea0
Matt Lewis <mlewis@greshamtech.com>
2017-12-21 17:57:00 +0000

    Add complete todos module tests

--- src/modules/todos.test.js
+++ src/modules/todos.test.js
@@ -1,18 +1,40 @@
 import configureMockStore from 'redux-mock-store';
+import { push } from 'react-router-redux';
 import thunk from 'redux-thunk';
-import reducer, { fetchTodos, FETCH_TODOS_SUCCESS, ADD_TODO } from './todos';
+import reducer, * as actions from './todos';
 
 describe('The Todos module', () => {
 
     describe('has an action creator which', () => {
-
         const mockStore = configureMockStore([ thunk ]);
 
-        it('adds a todo');
-        it('updates the status of a todo');
+        it('adds a todo', () => {
+            const todo = {
+                _id: 5,
+                text: 'A New Todo',
+                done: false
+            };
+            const expectedAction = {
+                type: actions.ADD_TODO,
+                payload: todo
+            }
+            expect(actions.addTodo(todo)).toEqual(expectedAction);
+        });
+        it('updates the status of a todo', () => {
+            const _id = 1;
+            const doneStatus = true;
+            const expectedAction = {
+                type: actions.UPDATE_DONE_STATUS,
+                payload: {
+                    _id,
+                    doneStatus
+                }
+            }
+            expect(actions.updateDoneStatus(_id, doneStatus)).toEqual(expectedAction);
+        });
         it('fetches todos, and gives notice of their arrival', () => {
             const expectedActions = [{
-                type: FETCH_TODOS_SUCCESS,
+                type: actions.FETCH_TODOS_SUCCESS,
                 payload: [
                     { _id: 1, text: 'Item One', done: false},
                     { _id: 2, text: 'Item Two', done: false},
@@ -22,12 +44,22 @@ describe('The Todos module', () => {
 
             const store = mockStore({ todos: [] });
 
-            // An actual async fetch request will need to be mocked here using a library like fetch-mock.
-            return store.dispatch( fetchTodos() ).then(() => {
+            // TODO An actual async fetch request will need to be mocked here using a library like fetch-mock.
+            return store.dispatch( actions.fetchTodos() ).then(() => {
                 expect( store.getActions() ).toEqual(expectedActions);
             });
         });
-        it('changes the filter');
+        it('changes the filter', () => {
+            const filter = 'DONE';
+            const expectedActions = [push(filter),{
+                type: actions.CHANGE_FILTER,
+                payload: filter
+            }];
+
+            const store = mockStore({ filter: 'ACTIVE'});
+            store.dispatch(actions.changeFilter(filter))
+            expect(store.getActions()).toEqual(expectedActions);
+        });
     });
 
     describe('has a reducer which', () => {
@@ -43,7 +75,7 @@ describe('The Todos module', () => {
         });
         it('should handle ADD_TODO', () => {
             expect(reducer(initialState, {
-                    type: ADD_TODO,
+                    type: actions.ADD_TODO,
                     payload: {
                         text: 'New item',
                         done: false
@@ -59,8 +91,52 @@ describe('The Todos module', () => {
                 }
             );
         });
-        it('should handle FETCH_TODOS_SUCCESS');
-        it('should handle UPDATE_DONE_STATUS');
-        it('should handle CHANGE_FILTER');
+        it('should handle FETCH_TODOS_SUCCESS', () => {
+            const payload = [
+                { _id: 1, text: 'Item One', done: false }
+            ];
+            expect(reducer(initialState, {
+                    type: actions.FETCH_TODOS_SUCCESS,
+                    payload
+                })
+            ).toEqual({
+                filter: initialState.filter,
+                listItems: payload
+            });
+        });
+        it('should handle UPDATE_DONE_STATUS', () => {
+            expect(reducer({
+                    filter: initialState.filter,
+                    listItems: [{
+                        _id: 1,
+                        text: 'An Item',
+                        done: false
+                    }]
+                }, {
+                    type: actions.UPDATE_DONE_STATUS,
+                    payload: {
+                        _id: 1,
+                        doneStatus: true
+                    }
+                })
+            ).toEqual({
+                filter: initialState.filter,
+                listItems: [{
+                    _id: 1,
+                    text: 'An Item',
+                    done: true
+                }]
+            })
+        });
+        it('should handle CHANGE_FILTER', () => {
+            expect(reducer(initialState, {
+                    type: actions.CHANGE_FILTER,
+                    payload: 'DONE'
+                })
+            ).toEqual({
+                filter: 'DONE',
+                listItems: initialState.listItems
+            });
+        });
     });
 });
