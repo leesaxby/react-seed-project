8026e048fc880a67c4ad167aa18df858c38f577c
Lee Saxby <lsaxby@gresham-computing.com>
2018-02-13 13:14:11 +0000

    Remove unused imports

--- src/pages/todos/ListRow.test.js
+++ src/pages/todos/ListRow.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import ListRow from './ListRow';
-import List from './listRow/List';
-import { Col, Row } from 'react-bootstrap';
 
 // Mock the nested "connected" components to avoid error with missing store.
 jest.mock('./listRow/List', () => 'List');
