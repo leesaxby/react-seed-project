3c285b0fc1c69d63ef5ab35499d55664a0edf8a2
Lee Saxby <lsaxby@gresham-computing.com>
2018-02-13 09:08:56 +0000

    Update header row component to use snapshot

--- src/pages/todos/HeaderRow.test.js
+++ src/pages/todos/HeaderRow.test.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import renderer from 'react-test-renderer';
 import HeaderRow from './HeaderRow';
 import { Row, Col, Image } from 'react-bootstrap';
 
 describe('A HeaderRow', () => {
 
-	it('renders without throwing an error', () => {
-		expect(shallow(<HeaderRow />).exists()).toBe(true);
+	it('renders component', () => {
+		const tree = renderer.create(
+			<HeaderRow />
+		).toJSON();
+
+		expect(tree).toMatchSnapshot();
 	});
 
-	it('renders a row that contains an image within a column', () => {
-		const wrapper = shallow(<HeaderRow />);
-
-		expect(wrapper.is(Row)).toBe(true);
-		expect(wrapper.childAt(0).is(Col)).toBe(true);
-		expect(wrapper.childAt(0).childAt(0).is(Image)).toBe(true);
-	});
-	
 });
