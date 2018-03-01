362f955143d0ee5fb0a989e1ae01fa7b63d28d2a
Will Kerswell <wkerswell@greshamtech.com>
2018-03-01 10:23:26 +0000

    Füge deutsch hinzu

--- i18n/translations/de.js
+++ i18n/translations/de.js
@@ -1,6 +1,6 @@
 export default {
     'todo.filter.todo': 'alles',
-    'todo.filter.showDone': 'Show done',
+    'todo.filter.showDone': 'Show fertig',
     'todo.filter.ariaLabel': 'filter auswählen',
     'todo.form.addItem': 'Artikel hinzufügen',
 };
