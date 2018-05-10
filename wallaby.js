9dcd019dac0c7031e5fe2d4c3796cdc1413f55cf
Will Kerswell <wkerswell@greshamtech.com>
2018-05-10 14:37:56 +0100

    Let wallaby know about the webpack aliases so it can build the code

--- wallaby.js
+++ wallaby.js
@@ -1,23 +1,28 @@
 module.exports = function (wallaby) {
-  return {
-    files: [
-      'src/**/*.js?(x)',
-      '!src/**/*test.js'
-    ],
+    return {
+        files: [
+            'src/**/*.js?(x)',
+            '!src/**/*test.js',
+        ],
 
-    tests: [
-      'src/**/*test.js'
-    ],
+        tests: [
+            'src/**/*test.js',
+        ],
 
-    env: {
-      type: 'node',
-      runner: 'node'
-    },
+        env: {
+            type: 'node',
+            runner: 'node',
+        },
 
-    compilers: {
-      '**/*.js?(x)': wallaby.compilers.babel()
-    },
-
-    testFramework: 'jest'
-  };
+        compilers: {
+            '**/*.js?(x)': wallaby.compilers.babel({
+                babelrc: true,
+                plugins: [['babel-plugin-webpack-alias', {
+                    config: `${wallaby.localProjectDir}/webpack/webpack.common.js`,
+                }],
+                ],
+            }),
+        },
+        testFramework: 'jest',
+    };
 };
