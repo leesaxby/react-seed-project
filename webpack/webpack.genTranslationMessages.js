2f043eac5c58cbda6da7a035f1479daeee81bf21
William Buss <wbuss@greshamtech.com>
2019-08-30 11:29:44 +0100

    Extra updates

--- webpack/webpack.genTranslationMessages.js
+++ webpack/webpack.genTranslationMessages.js
@@ -1,15 +1,12 @@
 const merge = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const common = require('./webpack.common.js');
+const prod = require('./webpack.prod.js');
 
 // NOTE: This Node env tells babel which plugins to use
 // This will gen all the messages that need to be translated
 process.env.NODE_ENV = 'gen-translations';
 
-module.exports = merge(common, {
-    output: {
-        filename: '[name].[chunkhash].js',
-    },
+module.exports = merge(prod, {
     plugins: [
         new CleanWebpackPlugin({
             cleanOnceBeforeBuildPatterns: [],
