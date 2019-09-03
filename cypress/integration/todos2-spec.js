9fbd19516be334c0d511f3b47629df666fe846ad
ahinorapl <apravcheva@greshamtech.com>
2019-09-03 11:25:05 +0100

    Add cypress dependency + test file

--- cypress/integration/todos2-spec.js
+++ cypress/integration/todos2-spec.js
@@ -1,3 +1,4 @@
+
 describe('Home Page', () => {
     beforeEach(() => {
         // eslint-disable-next-line no-undef
@@ -8,12 +9,29 @@ describe('Home Page', () => {
         cy.get('[data-test-id="todo-add-item"]').click();
     });
 
-    it('accepts input', () => {
+    it('accepts text input', () => {
         const text = 'some text';
 
         // eslint-disable-next-line no-undef
         cy.get('[type="text"]')
             .type(text)
             .should('have.value', text);
+    });
+    it('creates new todo', () => {
+        const text = 'some new text';
+        // eslint-disable-next-line no-undef
+        cy.get('[type="text"]')
+            .type(text)
+            .type('{enter}')
+            .should('have.value', '');
+
+        // eslint-disable-next-line no-undef
+        cy.get('[data-test-id="todo-item"]').should('have.length', 4);
+    });
+    it('checks of an item', () => {
+        // eslint-disable-next-line no-undef
+        cy.get('[data-test-id="todo-item"]').first().click();
+        // eslint-disable-next-line no-undef
+        cy.get('[data-test-id="todo-item"]').should('have.length', 2);
     });
 });
