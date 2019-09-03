d49069928604630c63dd6356d5f6d3dbb4d7bfff
ahinorapl <apravcheva@greshamtech.com>
2019-09-03 15:07:40 +0100

    Add run config for cypress test

--- run-e2e.sh
+++ run-e2e.sh
@@ -9,6 +9,7 @@ pid="$!"
 # Wait for server to be running.
 # FIX: hardcoded sleep 8 for the app to bootstrap.
 # while ! nc -z 0.0.0.0 8888; do sleep 30; done
+# shellcheck disable=SC1083
 while [[ "$(curl -s -o /dev/null -w %{http_code} 0.0.0.0:8888)" != "200" ]]; do
     sleep 5
 done
--- test/e2e/cypress/integration/todos.spec.js
+++ test/e2e/cypress/integration/todos.spec.js
@@ -1,36 +1,29 @@
 describe('Home Page', () => {
     beforeEach(() => {
-        // eslint-disable-next-line no-undef
-        cy.visit('http://localhost:9999');
+        cy.visit('/');
     });
     it('successfully clicks on Add Item ', () => {
-        // eslint-disable-next-line no-undef
         cy.get('[data-test-id="todo-add-item"]').click();
     });
 
     it('accepts text input', () => {
         const text = 'some text';
 
-        // eslint-disable-next-line no-undef
         cy.get('[type="text"]')
             .type(text)
             .should('have.value', text);
     });
     it('creates new todo', () => {
         const text = 'some new text';
-        // eslint-disable-next-line no-undef
         cy.get('[type="text"]')
             .type(text)
             .type('{enter}')
             .should('have.value', '');
 
-        // eslint-disable-next-line no-undef
         cy.get('[data-test-id="todo-item"]').should('have.length', 4);
     });
     it('checks off an item', () => {
-        // eslint-disable-next-line no-undef
         cy.get('[data-test-id="todo-item"]').first().click();
-        // eslint-disable-next-line no-undef
         cy.get('[data-test-id="todo-item"]').should('have.length', 2);
     });
 });
